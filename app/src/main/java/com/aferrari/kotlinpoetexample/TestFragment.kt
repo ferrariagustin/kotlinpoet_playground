828c230438ee57f99e188973800118021ba613a3
ferrariagustin <agustin.ferrari@mercadolibre.com>
2021-06-27 20:09:39 -0300

    change deeplink in TestFragment

--- app/src/main/java/com/aferrari/kotlinpoetexample/TestFragment.kt
+++ app/src/main/java/com/aferrari/kotlinpoetexample/TestFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.aferrari.annotation.AnnotationDeeplink
 
-@AnnotationDeeplink("test://test_fragment2")
+@AnnotationDeeplink("test://test_fragment")
 class TestFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
